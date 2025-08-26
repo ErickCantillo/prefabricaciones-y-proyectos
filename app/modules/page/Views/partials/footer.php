ea177fe432840e2c071b097333a5f4bbf90df3a9
Erick Cantillo <erick.cantillo97@gmail.com>
2025-08-26 12:09:40 -0500

    Añadir estilos de pie de página y enlaces de navegación para mejorar la presentación y la usabilidad

--- app/modules/page/Views/partials/footer.php
+++ app/modules/page/Views/partials/footer.php
@@ -3,188 +3,37 @@
     <div class="col-12">
         <img src="/images/LogoFooter.png" alt="Logo Alobien" class="img-fluid d-block m-auto">
       </div>
-    <div class="row gap-3 gap-lg-0">
+    <div class="row gap-3 gap-lg-0 mt-2">
       
       <?php $infopageModel = new Page_Model_DbTable_Informacion();
       $infopage = $infopageModel->getById(1);
       ?>
 
-
-      <div class="col-12 col-lg-4 text-center text-lg-center">
-        <h5>CONTÁCTO</h5>
+    <div class="col-12 col-lg-4">
+        <h5 class="footer-title">LINKS</h5>
         <div class="info">
-          <span class="info-footer d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-
-            <?php echo $infopage->info_pagina_correos_contacto ?>
-          </span>
-          <span class="info-footer d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-
-            <?php echo $infopage->info_pagina_telefono ?>
-          </span>
-
-        
-          <span class="info-footer d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-
-            <?php echo $infopage->info_pagina_direccion_contacto ?>
-          </span>
+          <ul class="footer-links">
+            <li><a href="#" class="footer-link">Inicio</a></li>
+            <li><a href="#" class="footer-link">Nosotros</a></li>
+            <li><a href="#" class="footer-link">Productos</a></li>
+            <li><a href="#" class="footer-link">Servicios</a></li>
+            <li><a href="#" class="footer-link">Galería</a></li>
+            <li><a href="#" class="footer-link">Clientes</a></li>
+            <li><a href="#" class="footer-link">Contáctenos</a></li>
+          </ul>
         </div>
       </div>
-      <div class="col-12 col-lg-4 text-center text-lg-center">
-        <h5>SÍGUENOS EN:</h5>
-        <div class="icons-redes d-flex text-center text-lg-center justify-content-center">
-
-          <?php if ($infopage->info_pagina_youtube) {
-            $youtube_data = $this->parseSocialLink($infopage->info_pagina_youtube);
-            if (!empty($youtube_data['url'])) { ?>
-              <a href="<?php echo $youtube_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-youtube"></i>
-                <!-- <?php echo !empty($youtube_data['user']) ? $youtube_data['user'] : $youtube_data['url']; ?> -->
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_instagram) {
-            $instagram_data = $this->parseSocialLink($infopage->info_pagina_instagram);
-            if (!empty($instagram_data['url'])) { ?>
-              <a href="<?php echo $instagram_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-instagram"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_facebook) {
-            $facebook_data = $this->parseSocialLink($infopage->info_pagina_facebook);
-            if (!empty($facebook_data['url'])) { ?>
-              <a href="<?php echo $facebook_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-facebook-f"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_linkedin) {
-            $linkedin_data = $this->parseSocialLink($infopage->info_pagina_linkedin);
-            if (!empty($linkedin_data['url'])) { ?>
-              <a href="<?php echo $linkedin_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-linkedin-in"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_x) {
-            $x_data = $this->parseSocialLink($infopage->info_pagina_x);
-            if (!empty($x_data['url'])) { ?>
-              <a href="<?php echo $x_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-x-twitter"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_pinterest) {
-            $pinterest_data = $this->parseSocialLink($infopage->info_pagina_pinterest);
-            if (!empty($pinterest_data['url'])) { ?>
-              <a href="<?php echo $pinterest_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-pinterest-p"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_flickr) {
-            $flickr_data = $this->parseSocialLink($infopage->info_pagina_flickr);
-            if (!empty($flickr_data['url'])) { ?>
-              <a href="<?php echo $flickr_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-flickr"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_tiktok) {
-            $tiktok_data = $this->parseSocialLink($infopage->info_pagina_tiktok);
-            if (!empty($tiktok_data['url'])) { ?>
-              <a href="<?php echo $tiktok_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-tiktok"></i>
-              </a>
-          <?php }
-          } ?>
-
+      <div class="col-12 col-lg-4">
+        <h5 class="footer-title">CONTACTO</h5>
+        <div class="info">
+          <a class="footer-link" href="tel:(601) 756 6621">Tel: (601) 756 6621</a>
         </div>
       </div>
-      <div class="col-12 col-lg-4 text-center text-lg-center">
-        <h5>SÍGUENOS EN:</h5>
-        <div class="icons-redes d-flex text-center text-lg-center justify-content-center">
 
-          <?php if ($infopage->info_pagina_youtube) {
-            $youtube_data = $this->parseSocialLink($infopage->info_pagina_youtube);
-            if (!empty($youtube_data['url'])) { ?>
-              <a href="<?php echo $youtube_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-youtube"></i>
-                <!-- <?php echo !empty($youtube_data['user']) ? $youtube_data['user'] : $youtube_data['url']; ?> -->
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_instagram) {
-            $instagram_data = $this->parseSocialLink($infopage->info_pagina_instagram);
-            if (!empty($instagram_data['url'])) { ?>
-              <a href="<?php echo $instagram_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-instagram"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_facebook) {
-            $facebook_data = $this->parseSocialLink($infopage->info_pagina_facebook);
-            if (!empty($facebook_data['url'])) { ?>
-              <a href="<?php echo $facebook_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-facebook-f"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_linkedin) {
-            $linkedin_data = $this->parseSocialLink($infopage->info_pagina_linkedin);
-            if (!empty($linkedin_data['url'])) { ?>
-              <a href="<?php echo $linkedin_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-linkedin-in"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_x) {
-            $x_data = $this->parseSocialLink($infopage->info_pagina_x);
-            if (!empty($x_data['url'])) { ?>
-              <a href="<?php echo $x_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-x-twitter"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_pinterest) {
-            $pinterest_data = $this->parseSocialLink($infopage->info_pagina_pinterest);
-            if (!empty($pinterest_data['url'])) { ?>
-              <a href="<?php echo $pinterest_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-pinterest-p"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_flickr) {
-            $flickr_data = $this->parseSocialLink($infopage->info_pagina_flickr);
-            if (!empty($flickr_data['url'])) { ?>
-              <a href="<?php echo $flickr_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-flickr"></i>
-              </a>
-          <?php }
-          } ?>
-
-          <?php if ($infopage->info_pagina_tiktok) {
-            $tiktok_data = $this->parseSocialLink($infopage->info_pagina_tiktok);
-            if (!empty($tiktok_data['url'])) { ?>
-              <a href="<?php echo $tiktok_data['url']; ?>" target="_blank" class="d-flex gap-2 align-items-center justify-content-center justify-content-lg-center">
-                <i class="fa-brands fa-tiktok"></i>
-              </a>
-          <?php }
-          } ?>
-
+      <div class="col-12 col-lg-4">
+        <h5 class="footer-title">DIRECCIÓN</h5>
+        <div class="text-start">
+          <p>Autop. Medellín un Km abajo rio Bogotá <br> (Occidente), <br>Cota Cundinamarca, Colombia</p>
         </div>
       </div>
       <div class="col-12">
