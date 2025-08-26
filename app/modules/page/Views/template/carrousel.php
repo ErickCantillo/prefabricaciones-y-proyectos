957c600c1e4ed6e7ff945d6ff8aa4e2d21b958b0
Erick Cantillo <erick.cantillo97@gmail.com>
2025-08-26 07:39:40 -0500

    Implement code changes to enhance functionality and improve performance

--- app/modules/page/Views/template/carrousel.php
+++ app/modules/page/Views/template/carrousel.php
@@ -1,5 +1,5 @@
 	<div class='carouselsection'>
-		<div class='left_scroll caja-control carrr'> <i class="fas fa-chevron-circle-left fa-2x"></i> </div>
+		<div class='left_scroll caja-control carrr'> <i class="fas fa-chevron-left"></i> </div>
 
 		<div class="carousel_inner carr">
 
@@ -14,5 +14,20 @@
 				<?php endforeach ?>
 			</ul>
 		</div>
-		<div class='right_scroll carrr caja-control'> <i class="fas fa-chevron-circle-right fa-2x"></i> </div>
+		<div class='right_scroll carrr caja-control'> <i class="fas fa-chevron-right"></i> </div>
 	</div>
+	
+	<script>
+	$(document).ready(function() {
+		$('.carouselsection').carousel({
+			quantity: 3,
+			sizes: {
+				'900': 3,
+				'600': 2,
+				'400': 1
+			},
+			pause: 5000,
+			auto: true
+		});
+	});
+	</script>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c3ce935cff16c8552db0e42a06857f6cf8eda925',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c3ce935cff16c8552db0e42a06857f6cf8eda925',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
